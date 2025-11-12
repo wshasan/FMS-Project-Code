1bf16ed82840f5d2aa82dad2232a8d4007bd2cdc
chalongk <chalongk@asu.edu>
2025-11-12 16:34:55 -0700

    Update Mountain.js

--- Mountain.js
+++ Mountain.js
@@ -9,8 +9,8 @@ function startMTGame() {
   backButton.show();   
   
   backButton.position(
-  (windowWidth - 1080) / 2 + 15,  
-  (windowHeight - 780) / 2 + 15
+  (windowWidth - 1080) / 2 + 15,  //+3
+  (windowHeight - 780) / 2 + 15   //+750
 );
 
   cnvMT = createCanvas(1080, 780);
